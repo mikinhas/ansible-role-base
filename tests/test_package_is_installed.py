e21377b9cf0a8cdf0ac13108d0da8a6693a8fb85
Michael MACHADO <michael.machado.reino@gmail.com>
2023-02-12 00:07:03 +0100

    Add Update package task

--- tests/test_package_is_installed.py
+++ tests/test_package_is_installed.py
@@ -1,3 +1,7 @@
+def test_update_all_packages(host):
+    check_return_false_in_changed_when_run_update_all_packages = host.ansible("apt", "upgrade=yes")["changed"]
+    assert check_return_false_in_changed_when_run_update_all_packages == False
+
 def test_vim_is_installed(host):
     nginx = host.package("vim")
     assert nginx.is_installed
