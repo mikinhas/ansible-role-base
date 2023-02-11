da765a3c6105cff9b33475bf6b0d5424438c7b33
Michael MACHADO <michael.machado.reino@gmail.com>
2023-02-12 00:28:52 +0100

    Rename test file & fix typo

--- tests/test_role_base.py
+++ tests/test_role_base.py
@@ -3,13 +3,13 @@ def test_update_all_packages(host):
     assert check_return_false_in_changed_when_run_update_all_packages == False
 
 def test_vim_is_installed(host):
-    nginx = host.package("vim")
-    assert nginx.is_installed
+    package = host.package("vim")
+    assert package.is_installed
 
 def test_htop_is_installed(host):
-    nginx = host.package("htop")
-    assert nginx.is_installed
+    package = host.package("htop")
+    assert package.is_installed
 
 def test_fail2ban_is_installed(host):
-    nginx = host.package("fail2ban")
-    assert nginx.is_installed
+    package = host.package("fail2ban")
+    assert package.is_installed
