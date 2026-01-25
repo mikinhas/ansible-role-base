783f87c2919f6035c3cc7f25b99a485c0c6f5118
Michael MACHADO <michael.machado.reino@gmail.com>
2026-01-25 22:29:35 +0100

    Update molecule tests

--- molecule/default/tests/test_role_base.py
+++ molecule/default/tests/test_role_base.py
@@ -1,27 +1,61 @@
+"""Testinfra tests for ansible-role-base."""
+
 import pytest
 
-def test_update_all_packages(host):
-    check_return_false_in_changed_when_run_update_all_packages = host.ansible("apt", "upgrade=yes")["changed"]
-    assert check_return_false_in_changed_when_run_update_all_packages == False
+
+def test_system_is_updated(host):
+    """Verify that system packages are up to date."""
+    result = host.ansible("apt", "upgrade=yes")
+    assert result["changed"] is False
 
 
 @pytest.mark.parametrize("package_name", [
-    "fail2ban",
+    "vim",
     "htop",
     "net-tools",
-    "vim",
 ])
-def test_if_all_packages_are_installed(host, package_name):
-    packages = host.package(package_name)
-    assert packages.is_installed
+def test_base_packages_are_installed(host, package_name):
+    """Verify that base packages are installed."""
+    package = host.package(package_name)
+    assert package.is_installed
 
 
-def test_fail2ban_sshd_jail_configuration_file_exists(host):
-    sshd_jail_configuration_file = host.file("/etc/fail2ban/jail.d/sshd.conf")
-    assert sshd_jail_configuration_file.exists
+def test_fail2ban_is_installed(host):
+    """Verify that fail2ban is installed."""
+    package = host.package("fail2ban")
+    assert package.is_installed
 
 
-def test_fail2ban_service(host):
+def test_fail2ban_service_is_running(host):
+    """Verify that fail2ban service is running and enabled."""
     service = host.service("fail2ban")
     assert service.is_running
     assert service.is_enabled
+
+
+def test_fail2ban_sshd_jail_file_exists(host):
+    """Verify that the sshd jail configuration file exists."""
+    jail_file = host.file("/etc/fail2ban/jail.d/sshd.conf")
+    assert jail_file.exists
+    assert jail_file.is_file
+    assert jail_file.user == "root"
+    assert jail_file.group == "root"
+    assert jail_file.mode == 0o644
+
+
+def test_fail2ban_sshd_jail_content(host):
+    """Verify the content of the sshd jail configuration."""
+    jail_file = host.file("/etc/fail2ban/jail.d/sshd.conf")
+    content = jail_file.content_string
+
+    assert "[sshd]" in content
+    assert "enable  = true" in content
+    assert "bantime = 86400" in content
+    assert "backend = systemd" in content
+
+
+def test_fail2ban_sshd_jail_is_active(host):
+    """Verify that the sshd jail is active in fail2ban."""
+    result = host.run("sudo fail2ban-client status sshd")
+    assert result.rc == 0
+    assert "sshd" in result.stdout
